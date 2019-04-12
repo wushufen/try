4b091c4c91170a03586134000a454d29a76e67c8
wushufen <wushufen@sunlands.com>
2019-04-12 17:58:01 +0800

    .

---
+++ eslint/test.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>2</h1>
+    <table>
+      <tr>
+        <td>1</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+var str = 'string'
+var s = 's'
+if (s == 's') {
+  a
+}
+export default {
+  data() {
+    return {list: []}
+  }
+}
+</script>
+<style>
+.x {
+width: 1px;
+}
+</style>
+<style lang="less" scoped>
+.x {
+.x {
+width: 1px;
+}
+}
+</style>
+<style lang="scss" scoped>
+.x {
+.x {
+width: 1px;
+}
+}
+</style>
