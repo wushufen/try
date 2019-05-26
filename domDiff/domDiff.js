4acc3c8c8a4d7b7fea192011c5d6e7ad145e00fe
wusfen <wusfun@foxmail.com>
2019-05-27 02:23:55 +0800

    vm

--- domDiff/domDiff.js
+++ domDiff/domDiff.js
@@ -1,46 +1,78 @@
+// 从零实现mvvm框架，重写一个轻量的vue并兼容ie6
+
 {
-  function toArray(arrayLike) {
-    var array = [], i = arrayLike.length
-    while (i--) {
-      array[i] = arrayLike[i]
+  if (!window.requestAnimationFrame) {
+    var requestAnimationFrame = function (fn) {
+      setTimeout(fn, 0)
     }
-    return array
-  }
-
-  function toJson(obj) {
-    return JSON.stringify(obj, null, '  ')
-  }
-
-  function extend(obj, obj2) {
-    return Object.assign(obj, obj2)
+    var cancelAnimationFrame = function (timer) {
+      clearTimeout(timer)
+    }
   }
 
   function forEach(arrayLike, fn) {
-    var array = []
     for (var i = 0; i < arrayLike.length; i++) {
-      var value = fn.call(this, arrayLike[i], i)
-      array.push(value)
+      fn.call(this, arrayLike[i], i)
     }
-    return array
   }
 
   function each(list, fn) {
-    var array = []
-    if (list instanceof Array) {
-      for (var i = 0; i < list.length; i++) {
-        var rs = fn.call(this, list[i], i, i)
+    var array = [], i = 0, rs
+    if (list instanceof Array || typeof list === 'string') {
+      while (i < list.length) {
+        rs = fn.call(this, list[i], i, i++)
+        array.push(rs)
+      }
+    } else if (typeof list === 'number') {
+      while (i++ < list) {
+        rs = fn.call(this, i, i, i)
         array.push(rs)
       }
     } else {
-      var i2 = 0
       for (var key in list) {
         if (list.hasOwnProperty(key)) {
-          var rs2 = fn.call(this, list[key], key, i2++)
-          array.push(rs2)
+          rs = fn.call(this, list[key], key, i++)
+          array.push(rs)
         }
       }
     }
     return array
+  }
+
+  function assign(obj, obj2) {
+    each(obj2, function (value, key) {
+      obj[key] = value
+    })
+    return obj
+  }
+
+  function toArray(arrayLike) {
+    var array = [], i = arrayLike.length
+    while (i--) array[i] = arrayLike[i]
+    return array
+  }
+
+  function toJson(val) {
+    if (typeof val === 'string') {
+      return '"' + val + '"'
+    }
+    if (/number|boolean/.test(typeof val) || val === null) {
+      return String(val)
+    }
+    if (val instanceof Array) {
+      var arr = []
+      forEach(val, function (item) {
+        arr.push(toJson(item))
+      })
+      return '[' + arr.join(',\n') + ']'
+    }
+    if (typeof val === 'object') {
+      var items = []
+      each(val, function (item, key) {
+        items.push('"' + key + '": ' + toJson(item))
+      })
+      return '{' + items.join(',\n') + '}'
+    }
   }
 
   function parse(html) {
@@ -49,10 +81,15 @@
     return parse.el.children[0]
   }
 
-  function getDirs(node) {
-    var dirs = []
+  function getNodeData(node) {
+    var data = {
+      ns: node.namespaceURI,
+      attrs: {},
+      directives: []
+    }
     var attributes = toArray(node.attributes)
     forEach(attributes, function (attribute) {
+      if (!attribute.specified) return // ie
       var attr = attribute.nodeName
       var value = attribute.nodeValue
       // v-bind:title  :title  v-on:click  @click.prevent.stop
@@ -72,54 +109,73 @@
           arg: arg,
           mdfs: mdfs
         }
-        if (/^(pre|for|if|else-if|else|model|is)$/.test(name)) {
-          dirs[name] = dir
-        } else {
-          dirs.push(dir)
-        }
 
+        if (name === 'on') {
+          if (value.match(/[=();]/)) {
+            dir.value = 'function(){' + value + '}'
+          } else {
+            dir.value = 'function(){' + value + '.apply(__vm,arguments)}'
+          }
+        }
+        if (name === 'model') {
+          dir.setModel = 'function(value){' + value + '=value; __vm.$render()}'
+        }
         if (name === 'for') {
-          m = value.match(/(?:\(([^,]+),(\S+?)\)|(\S+))\s+in\s+(\S+)/)
+          // (item, i)|item    in|of    list
+          m = value.match(/(?:\(([^,]+),(\S+?)\)|(\S+))\s+(?:in|of)\s+(\S+)/)
           dir.item = m[1] || m[3]
           dir.index = m[2] || '$index'
           dir.list = m[4]
         }
+
+        if (/^(for|if)$/.test(name)) {
+          data.directives[name] = dir
+        } else {
+          data.directives.push(dir)
+        }
+      } else {
+        data.attrs[attr] = value
       }
     })
-    return dirs
+    return data
   }
 
-  function getAttrsJson(node) {
-    var lines = []
-    forEach(node.attributes, function (attribute) {
-      var nodeName = attribute.nodeName
-      var nodeValue = attribute.nodeValue
-      lines.push('"' + nodeName + '": "' + nodeValue + '"')
-    })
-    return '{' + lines.join(',\n') + '}'
-  }
-
-  function createElement(tag, options, childNodes) {
-    options = options || {}
-    var node = document.createElement(tag)
-
-    if (options instanceof Array) {
-      childNodes = options
-      options = {}
+  function createElement(tag, data, childNodes) {
+    tag = tag.toLowerCase()
+    data = data || {}
+    if (data instanceof Array) {
+      childNodes = data
+      data = {}
     }
 
-    if (options.attrs) {
-      for (var attr in options) {
-        node.setAttribute(attr, options[attr])
+    // namespaceURI
+    var node = data.ns && document.createElementNS
+      ? document.createElementNS(data.ns, tag)
+      : document.createElement(tag)
+
+    // attrs
+    if (data.attrs) {
+      for (var attr in data.attrs) {
+        node.setAttribute(attr, data.attrs[attr])
       }
     }
 
+    // directives
+    if (data.directives) {
+      forEach(data.directives, function (directive) {
+        var name = directive.name
+        var fn = VM.options.directives[name].bind
+        fn(node, directive)
+      })
+    }
+
+    // childNodes
     if (childNodes) {
       forEach(childNodes, function (childNode) {
         if (childNode instanceof Array) {
           forEach(childNode, function (childNode) {
-            if (typeof childNode === 'string') {
-              childNode = document.createTextNode(childNode)
+            if (typeof childNode !== 'object') {
+              childNode = document.createTextNode(String(childNode))
             }
             node.appendChild(childNode)
           })
@@ -134,6 +190,8 @@
     return node
   }
 
+  var __createElement = createElement
+  var __each = each
   function compile(node) {
     /*
     createElement('div', {}, [
@@ -152,21 +210,28 @@
       if (!code.match(/^$|\[\s*$/)) code += ',\n' // [childNode, ..]
 
       if (node.nodeType === 1) {
-        var dirs = getDirs(node)
+        var nodeData = getNodeData(node)
+        var nodeDataJson = toJson(nodeData)
+        var dirs = nodeData.directives
+
+        nodeDataJson = nodeDataJson.replace(/"value":\s*"((?:\\.|.)*?)"/g, 'value: $1')
+        nodeDataJson = nodeDataJson.replace(/"setModel":\s*"((?:\\.|.)*?)"/g, 'setModel: $1')
+
+
         // for if?
         // forEach(,()=> bool? createElement(,,[..loop..]): "" )
         if (dirs['for']) {
           var dir = dirs['for']
-          code += 'this.forEach(' + dir.list + ',function(' + dir.item + ',' + dir.index + '){return '
+          code += 'this.__each(' + dir.list + ',function(' + dir.item + ',' + dir.index + '){return '
         }
         // if
         // bool? createElement(,,[..loop..]): ""
         if (dirs['if']) {
           code += dirs['if'].value + '? '
         }
+
         // createElement
-        var attrsJson = getAttrsJson(node)
-        code += 'this.createElement("' + node.tagName + '", ' + attrsJson + ', [\n'
+        code += 'this.__createElement("' + node.tagName + '", ' + nodeDataJson + ', [\n'
 
         // childNodes
         forEach(toArray(node.childNodes), function (childNode) {
@@ -218,14 +283,15 @@
     }
     // *text
     if (dom.nodeType === 3) {
-      if (dom.textContent !== newDom.textContent) {
-        dom.textContent = newDom.textContent
+      if (dom.nodeValue !== newDom.nodeValue) {
+        dom.nodeValue = newDom.nodeValue
       }
       return
     }
     // *attr
-    var attributes = toArray(dom.attributes).concat(toArray(newDom.attributes))
+    var attributes = toArray(newDom.attributes)//.concat(toArray(dom.attributes))
     forEach(attributes, function (attribute) {
+      if (!attribute.specified) return // ie
       var attr = attribute.nodeName
       var value = dom.getAttribute(attr)
       var newValue = newDom.getAttribute(attr)
@@ -233,6 +299,9 @@
         dom.removeAttribute(attr)
       } else if (value !== newValue) {
         dom.setAttribute(attr, newValue)
+        if(attr === 'class'){ // ie
+          dom.className = newValue
+        }
       }
     })
     // *children
@@ -246,38 +315,175 @@
 
   function injectRender(vm, fn) {
     var $fn = function () {
-      fn.apply(vm, arguments)
+      var restore = injectRenderToAsyncs(vm)
+      fn.apply(this, arguments)
+      restore()
+
       vm.$render()
     }
     return $fn
   }
 
+  function injectRenderToAsyncs(vm) {
+    var setTimeout = window.setTimeout
+    window.setTimeout = function () {
+      var fn = arguments[0]
+      arguments[0] = injectRender(vm, fn)
+      setTimeout.apply(this, arguments)
+    }
+
+    var setInterval = window.setInterval
+    window.setInterval = function () {
+      var fn = arguments[0]
+      arguments[0] = injectRender(vm, fn)
+      setInterval.apply(this, arguments)
+    }
+
+    var XMLHttpRequest = window.XMLHttpRequest || window.ActiveXObject
+    var XHRprototype = XMLHttpRequest.prototype
+    var send = XHRprototype.send
+    XHRprototype.send = function () {
+      var xhr = this
+      each(xhr, function (handler, name) {
+        if (name.match(/^on/) && typeof handler === 'function') {
+          xhr[name] = injectRender(vm, handler)
+        }
+      })
+      return send && send.apply(xhr, arguments)
+    }
+
+    return function () {
+      window.setTimeout = setTimeout
+      window.setInterval = setInterval
+      XHRprototype.send = send
+    }
+  }
+
   function VM(options) {
     var vm = this
     vm.$options = options || (options = {})
+
+    // el || template
     vm.$el = options.el || parse(options.template)
 
     // render
-    var render = new Function('with(this){return ' + compile(vm.$el) + '}')
+    var code = compile(vm.$el)
+    var render = new Function('var __vm=this;with(__vm){return ' + code + '}')
+    var timer
     vm.$render = function () {
-      var newDom = render.call(vm)
-      diff(vm.$el, newDom)
+      // update computed
+      each(options.computed, function (fn, key) {
+        vm[key] = fn.call(vm)
+      })
+
+      // update view
+      cancelAnimationFrame(timer)
+      timer = requestAnimationFrame(function () {
+        var newDom = render.call(vm)
+        diff(vm.$el, newDom)
+      })
     }
     vm.$render.render = render
+    vm.$el.innerHTML = ''
 
     // data
-    extend(vm, options.data)
+    var data = options.data
+    if (typeof data === 'function') data = data.call(vm)
+    assign(vm, data)
 
     // methods
     each(options.methods, function (fn, key) {
-      console.log(key, fn)
       vm[key] = injectRender(vm, fn)
     })
 
+    // computed
+
+    // watch
+
+    // created
+
+    // mounted
+    if (options.mounted) {
+      vm.mounted = injectRender(vm, options.mounted)
+      setTimeout(function () {
+        vm.mounted()
+      }, 0)
+    }
+
+    vm.$render()
+
+    // proxy
+    // if (typeof Proxy === 'function') {
+    //   return new Proxy(vm, {
+    //     set: function (vm, key, val) {
+    //       vm[key] = val
+    //       vm.$render()
+    //     },
+    //     get: function (vm, key) {
+    //       var val = vm[key]
+    //       vm.$render()
+    //       return val
+    //     }
+    //   })
+    // }
+  }
+
+  VM.prototype = {
+    constructor: VM,
+    __createElement: createElement,
+    __each: each
+  }
+
+  VM.options = {
+    directives: {}
+  }
+
+  VM.directive = function (name, definition) {
+    if (typeof definition === 'function') {
+      definition = {
+        bind: definition,
+        update: definition
+      }
+    }
+    VM.options.directives[name] = definition
+  }
+
+  // props
+  VM.directive('bind', function (el, binding) {
+    var arg = binding.arg, value = binding.value
+    if (arg === 'class') {
+      each(value, function (bool, key) {
+        if (bool) {
+          el.className += el.className ? ' ' + key : key
+        } else {
+          el.className = el.className.replace(RegExp('^|\\s+'+key, 'g'), '')
+        }
+      })
+      return
+    }
+    if (arg === 'style') {
+      assign(el.style, value)
+      return
+    }
+    el.setAttribute(arg, String(value))
+    // el[binding.arg] = binding.value  // todo: bind is props, not attribute
+  })
+
+  VM.directive('on', function (el, binding) {
+    el['on' + binding.arg] = function (e) {
+      binding.value(e)
+    }
+  })
+
+  VM.directive('model', function (el, binding) {
+    el.value = binding.value
+    el.onkeydownx = el.onkeyup = el.oninput = function () {
+      binding.setModel(el.value)
+    }
+  })
+
+  VM.compoment = function (name, definition) {
 
   }
-  VM.prototype = {
-    createElement: createElement,
-    forEach: forEach
-  }
+
 }
